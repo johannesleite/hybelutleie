fd05b73071ce66ef591bb3a324011e6fe43835d3
johannesleite <leite.johannes@gmail.com>
2022-11-06 22:32:40 +0100

    begynt å utforme card for alle annonser

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ include(INC_PATH . '/header.php');
             <div class="card mb-3">
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="assets/img/159_196037135.jpg" class="img-fluid rounded-start" alt="...">
+                        <img src="<?php echo $row['ad_image']; ?>" class="img-fluid rounded-start" alt="...">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
